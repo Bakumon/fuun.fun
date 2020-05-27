24e086a50f05fdadac0548ed6a95c6a8803d64e7
Bakumon <bakumon.me@gmail.com>
2020-05-27 11:07:27 +0800

    update

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    //mobile menu toggling
-    $("#menu_icon").click(function(){
-        $("header nav ul").toggleClass("show_menu");
-        $("#menu_icon").toggleClass("close_menu");
-        return false;
-    });
-
-});
